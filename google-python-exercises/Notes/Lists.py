bb69b8d7cbf7691d42a75bf7b69eff69b8fc5c70
lalasparrow <huangshenglan1994@gmail.com>
2018-01-30 11:39:38 -0800

    to While Loop

---
+++ google-python-exercises/Notes/Lists.py
@@ -0,0 +1,14 @@
+Lists
+
+Contents
+	1. list 也用len()算长度，第一个element的index是0
+		  colors = ['red', 'blue', 'green']
+		  print colors[0]    ## red
+		  print colors[2]    ## green
+		  print len(colors)  ## 3
+
+
+							_____ ______ _______
+		  colors   ->      |'red'|'blue'|'green'|
+		  					----- ------ -------
+		  			list	  0      1      2
